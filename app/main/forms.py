6b1c3d86d97168a5b62667f7c4c7a13534396ffb
AdnanK1 <adnangitonga@gmail.com>
2022-05-09 00:38:24 +0300

    Added validation check on the username and email

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,9 @@
+import email
+from xml.dom import ValidationErr
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField,SubmitField
-from wtforms.validators import Length, EqualTo, Email, DataRequired
+from wtforms.validators import Length, EqualTo, Email, DataRequired,ValidationError
+from ..models import User
 
 class RegisterForm(FlaskForm):
     username = StringField(label='Username:',validators=[Length(min=2, max=25), DataRequired()])
@@ -8,5 +11,17 @@ class RegisterForm(FlaskForm):
     password1 = PasswordField(label='Password:', validators=[Length(min=2), DataRequired()])
     password2 = PasswordField(label='Confirm Password:',validators=[EqualTo('password1'), DataRequired()])
     submit = SubmitField(label='Create Account')
+
+    def validate_username(self, username_to_check):
+        user = User.query.filter_by(username=username_to_check.data).first()
+        if user:
+            raise ValidationError('Username already exist!')
+
+    def validate_email(self, email_to_check):
+        email = User.query.filter_by(email=email_to_check.data).first()
+        if email:
+            raise ValidationError('Email Address already exist!')
+
+
 
 
