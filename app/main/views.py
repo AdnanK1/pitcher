e04f17302b12346f2c687b8f45c3127988c3691e
AdnanK1 <adnangitonga@gmail.com>
2022-05-08 22:58:04 +0300

    added validators to the forms.

--- app/main/views.py
+++ app/main/views.py
@@ -15,9 +15,9 @@ def home_page():
 def register_page():
     form = RegisterForm()
     if form.validate_on_submit():
-        user_to_create = User(username=form.username.data,email=form.email.data,password=form.password1.data)
+        user_to_create = User(username=form.username.data,email=form.email.data,password_hash=form.password1.data)
         db.session.add(user_to_create)
         db.session.commit()
         return redirect(url_for('home_page'))
-        
+
     return render_template('register.html', form=form)
