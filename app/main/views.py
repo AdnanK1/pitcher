90e300625373651c71bedbb7d2b3d78d8426cdc1
AdnanK1 <adnangitonga@gmail.com>
2022-05-08 17:33:40 +0300

    the form is used to enter data into the database.

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,9 @@
+import email
 from flask import render_template,redirect,url_for
 from . import main
 from .forms import RegisterForm
+from ..models import User
+from . import db
 
 @main.route('/')
 @main.route('/home')
@@ -8,7 +11,12 @@ def home_page():
 
     return render_template('home.html')
 
-@main.route('/register')
+@main.route('/register', methods=['GET','POST'])
 def register_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data,email=form.email.data,password=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('home_page'))
     return render_template('register.html', form=form)
