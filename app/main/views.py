4744ae354118054576add8ef6b4c13e40563fded
AdnanK1 <adnangitonga@gmail.com>
2022-05-08 15:03:58 +0300

    added routing to register.html.

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
 from flask import render_template,redirect,url_for
 from . import main
+from main.forms import RegisterForm
 
 @main.route('/')
 @main.route('/home')
@@ -7,3 +8,7 @@ def home_page():
 
     return render_template('home.html')
 
+@main.route('/register')
+def register_page():
+    form = RegisterForm()
+    return render_template('register.html', form=form)
