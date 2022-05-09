e6cd2b892925e78793129ba6b80f32c5e99b422a
AdnanK1 <adnangitonga@gmail.com>
2022-05-09 22:09:43 +0300

    Deploy to heroku.

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-
+import os
 
 class Config:
     
@@ -14,7 +14,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
 class DevConfig(Config):
